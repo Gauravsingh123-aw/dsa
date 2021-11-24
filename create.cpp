937afed02c518d2b40f52ec1fb1b33c8946ffbb6
Gaurav Singh <gauravsinghaw123@gmail.com>
2021-11-24 23:37:15 +0530

    updating create.cpp

--- create.cpp
+++ create.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namespace std;
+
+//creating a node using class
+
 class node{
     public:
     int data;
@@ -10,11 +13,52 @@ class node{
         next=NULL;
     }
 };
-void insertattail(node* head, int val){
-   node* n =new node(val);
+
+//function to insert a node at last
+
+void createnode(node* &head,int val)
+{
+    node* n= new node(val);
+    node* temp=head;
+    if(head==NULL)
+        head=n;
+    else
+    {
+        while(temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        temp->next=n;
+    }
 }
+ //function to display the linkedlist
+
+ void display(node* &head)
+ {
+    node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->"<<" ";
+        temp=temp->next;
+    }
+    cout<<"NULL";
+ }
 int main()
 {
+    node* head=NULL;
+    int n,value;
+    cout<<"How many element you want to store in linked list";
+    cin>>n;
+    while(n--)
+    {
+        cout<<"Enter the value";
+        cin>>value;
+        createnode(head,value);
+    }
+
+
+
+    display(head);
 
 return 0;
 }
