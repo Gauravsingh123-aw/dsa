af8a25347f59f72273e19a0f0354d168e0edc584
Gaurav Singh <gauravsinghaw123@gmail.com>
2021-11-25 00:23:10 +0530

    updating by insertions at various positions

--- create.cpp
+++ create.cpp
@@ -14,9 +14,43 @@ class node{
     }
 };
 
+//function to insert node at beginning
+
+void insertatbeg(node* &head,int val)
+{
+    node* n=new node(val);
+    node* temp=head;
+    if(head==NULL)
+    {
+        head=n;
+    }
+    else{
+    head=n;
+    head->next=temp;
+    }
+
+}
+//function to insert node in middle
+void insertatmid(node* &head,int val,int pos)
+{
+    node* n=new node(val);
+    node* temp=head;
+    node* p=head;
+    int c=1;
+    while(c!=pos)
+    {
+        temp=temp->next;
+        p=p->next;
+        c++;
+    }
+    p=temp->next;
+    temp->next=n;
+    n->next=p;
+
+}
 //function to insert a node at last
 
-void createnode(node* &head,int val)
+void insertatlast(node* &head,int val)
 {
     node* n= new node(val);
     node* temp=head;
@@ -41,24 +75,56 @@ void createnode(node* &head,int val)
         cout<<temp->data<<"->"<<" ";
         temp=temp->next;
     }
-    cout<<"NULL";
+    cout<<"NULL"<<endl;
  }
 int main()
 {
     node* head=NULL;
-    int n,value;
-    cout<<"How many element you want to store in linked list";
-    cin>>n;
-    while(n--)
+    int n=1,value,choice;
+    int pos;
+    while(n>0)
     {
-        cout<<"Enter the value";
-        cin>>value;
-        createnode(head,value);
+    cout<<"where do you want to insert the node in linkedlist"<<endl;
+    cout<<"Press 1 to insert at beginning"<<endl;
+    cout<<"Press 2 to insert at middle"<<endl;
+    cout<<"press 3 to insert at last"<<endl;
+    cin>>choice;
+
+    switch(choice){
+        case 1:
+        {
+            cout<<"enter the value";
+            cin>>value;
+            insertatbeg(head,value);
+        }
+        break;
+        case 2:
+        {
+            cout<<"enter the element and position";
+            cin>>value>>pos;
+            if(head==NULL)
+                cout<<"not possible";
+            insertatmid(head,value,pos);
+        }
+        break;
+        case 3:
+        {
+            cout<<"enter the value";
+            cin>>value;
+            insertatlast(head,value);
+        }
+        break;
+        default:
+        cout<<"Wrong choice"<<endl;
+
+
+    }
+    display(head);
+    cout<<"press 0 if you want to exit";
+    cin>>n;
     }
 
 
-
-    display(head);
 
 return 0;
 }
