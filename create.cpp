09b256f9cc8c970bd243e74cb49fcdf2012e4b3a
Gaurav Singh <gauravsinghaw123@gmail.com>
2021-11-24 22:41:01 +0530

    create.cpp added

---
+++ create.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+void insertattail(node* head, int val){
+   node* n =new node(val);
+}
+int main()
+{
+
+return 0;
+}
